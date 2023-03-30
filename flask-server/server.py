6c620b06e38ec8183799a0f2d60c94f18611e751
devkartikrathi <kartikrathi0808@gmail.com>
2023-03-30 17:56:29 +0530

    server

--- flask-server/server.py
+++ flask-server/server.py
@@ -6,5 +6,5 @@ app.route("/members")
 def members():
     return {"members" : ["member1", "member2", "member3"]}
 
-if "__name__" == "__main__":
+if __name__ == "__main__":
     app.run(debug = True)
