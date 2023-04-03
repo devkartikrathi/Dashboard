361151f46d4e7f516a6e5956463d623e614f6a72
rianeps <97471659+rianeps@users.noreply.github.com>
2023-04-03 11:26:35 +0530

    Login routes

--- flask-server/app.py
+++ flask-server/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template
 from flask_cors import CORS
 from database import *
 
@@ -7,8 +7,35 @@ CORS(app)
 
 @app.route('/') # type: ignore
 def INDEX_1():
-    pass
+    return render_template('index.html')
 
+@app.route('/register', methods = ['POST'])
+def register():
+    username = request.form['username']
+    password = request.form['password']
+    remember = request.form.get('remember', False)
+
+    min_user_len = 5
+    username_req = [
+        lambda s: any(x.isupper() for x in s), 
+        lambda s: any(x.islower() for x in s),
+        lambda s: any(x.isdigit() for x in s) 
+    ]
+
+    if len(username) < min_user_len or not all(req(username) for req in username_req):
+        error_mess = 'Invalid username! Username must be 5-15 characters, a-z, A-Z and 0-9!'
+
+    minpasslen = 5
+
+    if len(password) < minpasslen:
+        error_msg = 'Password must be between 5-15 characters!'
+
+@app.route('/login', methods = ['POST'])
+def login():
+    username = request.form['username']
+    password = request.form['password']
+    remember = request.form.get('remember', False)
+    
 @app.route('/data', methods=['GET'])
 def GET_DATA():
     data = get_data()
