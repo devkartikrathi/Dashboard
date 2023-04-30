b7d18140f30363243324e022de8e2e008e0b97aa
devkartikrathi <kartikrathi0808@gmail.com>
2023-04-30 12:13:55 +0530

    test.py > app.py

--- flask-server/app.py
+++ flask-server/app.py
@@ -1,88 +1,146 @@
-from flask import Flask, jsonify, request, render_template
-from flask_jwt_extended import JWTManager, jwt_required, create_access_token, get_jwt_identity, get_jwt
-from datetime import timedelta
-from werkzeug.security import generate_password_hash, check_password_hash
+from flask import Flask, jsonify, request, render_template, redirect, url_for, make_response
+import jwt
+import datetime
+from functools import wraps
 from flask_cors import CORS
 from database import *
 
 app = Flask(__name__)
 CORS(app)
+app.config['SECRET_KEY'] = 'your_secret_key'
 
-app.config['JWT_SECRET_KEY'] = 'super-secret'
-app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(hours=1)
-jwt = JWTManager(app)
+# Decorator for authentication
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = None
 
-teachers = [
-    {'id': 1, 'name': 'John Doe', 'subject': 'Math'},
-    {'id': 2, 'name': 'Jane Smith', 'subject': 'English'},
-    {'id': 3, 'name': 'Bob Johnson', 'subject': 'Science'},
-]
+        if 'x-access-token' in request.headers:
+            token = request.headers.get('x-access-token')
 
-@app.route('/') # type: ignore
-def INDEX_1():
-    return render_template('index.html')
+        if not token:
+            return jsonify({'message': 'Token is missing!'}), 401
 
-@app.route('/admin/login', methods=['POST'])
-def admin_login():
-    # Replace with your own authentication logic
+        try:
+            #data = jwt.decode(token, app.config['SECRET_KEY'])
+            # current_user = users.find_one({'role': data['username']})
+            current_user = {'role' : 'admin'}
+            print('done')
+        except:
+            return jsonify({'message': 'Token is invalid!'}), 401
+
+        return f(current_user, *args, **kwargs)
+
+    return decorated
+
+# Login route
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template('login.html')
+
+# Authenticate user route
+@app.route('/auth', methods=['POST'])
+def auth():
     username = request.form['username']
     password = request.form['password']
+    user = check_user(username=username, password=password)
+    if not user:
+        return jsonify({'message': 'Invalid username or password!'})
+    token_ = jwt.encode({'username': 'admin', 'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30)}, app.config['SECRET_KEY'])
 
-    if username != 'admin' or password != 'admin':
-        return jsonify({'msg': 'Invalid username or password'}), 401
+    return jsonify({'token': token_})
+    # resp = make_response(redirect(url_for('admin_dashboard')))
+    # resp.headers['x-access-token'] = token_
+    # return resp
+    # return redirect(url_for('admin'), headers={'x-access-token': token})
 
-    access_token = create_access_token(identity=username, additional_claims={'role': 'admin'})
-    return jsonify({'access_token': access_token}), 200
+# Admin dashboard route
+# @app.route('/admin', methods=['GET', 'POST'])
+# @token_required
+# def admin_dashboard(current_user):
+#     if current_user['role'] != 'admin':
+#         return jsonify({'message': 'You do not have access to this page!'})
 
-# def get_user_role():
-#     claims = get_jwt()
-#     return claims['role'] if claims else None
-
-@app.route('/teachers', methods=['POST'])
-@jwt_required(optional=True)
-def add_teacher():
-    claims = get_jwt()
-    if claims['role'] != 'admin':
-        return jsonify({'msg': 'Admin privileges required'}), 403
-
-    teacher = {'id': len(teachers) + 1, 'name': request.form['name'], 'subject': request.form['subject']}
-    teachers.append(teacher)
-
-    return jsonify({'msg': 'Teacher added successfully'}), 200
-
-@app.route('/teachers', methods=['GET'])
-@jwt_required(optional=True)
-def get_teachers():
-    teachers_list = [{'name': t['name'], 'subject': t['subject']} for t in teachers]
-    return jsonify(teachers_list), 200
-
-# @app.route('/login', methods = ['GET', 'POST']) # type: ignore
-# def login():
 #     if request.method == 'POST':
-#         username = request.form['username']
-#         password = request.form['password']
+#         name = request.form['name']
+#         description = request.form['description']
+#         teacher_id = request.form['teacher_id']
 
-#         if not username or not password:
-#             return jsonify({'msg': 'Invalid credentials'}), 400
+#         course = {'name': name, 'description': description, 'teacher_id': teacher_id}
+#         #courses.insert_one(course)
 
-#         user = check_user(username=username)
-#         if not user or not check_password_hash(user['password'], password):
-#             return jsonify({'msg': 'Invalid credentials'}), 401
+#     courses_list = [1, 2, 3]
 
-#         access_token = create_access_token(identity=user['_id'], expires_delta=timedelta(hours=1), additional_claims={'role': user['role']})
-#         return jsonify({'access_token': access_token}), 200
+#     return render_template('admin.html', courses=courses_list)
 
-#     if request.method == 'GET' or 'POST':
-#         return jsonify(course_data())
+# Teacher dashboard route
+# @app.route('/teacher', methods=['GET'])
+# @token_required
+# def teacher_dashboard(current_user):
+#     if current_user['role'] != 'teacher':
+#         return jsonify({'message': 'You do not have access to this page!'})
 
-def refresh():
-    current_user = get_jwt_identity()
-    access_token = create_access_token(identity=current_user, expires_delta=timedelta(hours=1), additional_claims={'role': 'user'})
-    return jsonify({'access_token': access_token}), 200
+#     courses_list = list(courses.find({'teacher_id': str(current_user['_id'])}))
 
-@app.errorhandler(401)
-def handle_unauthorized_error(error):
-    return jsonify({'msg': 'Missing or invalid access token'}), 401
+#     return render_template('teacher.html', courses=courses_list)
 
+# Add course
+@app.route('/addcourse', methods=['POST']) # type: ignore
+@token_required
+def add_course(current_user):
+    if current_user['role'] != 'admin':
+        return jsonify({'message': 'You do not have access to this page!'})
+
+    if request.method == 'POST':
+        program = request.form['program']
+        course_code = request.form['course_code']
+        course_name = request.form['course_name']
+        course_type = request.form['course_type']
+
+        course = {'program': program, 'course_code': course_code, 'course_name': course_name, 'course_type': course_type}
+        #courses.insert_one(course)
+        return jsonify({'course': course})
+
+# Add members
+@app.route('/addmember', methods=['POST']) # type: ignore
+@token_required
+def add_member(current_user): # type: ignore
+    if current_user['role'] != 'admin':
+        return jsonify({'message': 'You do not have access to this page!'})
+
+    course_code = request.form['course_code']
+    course_name = request.form['course_name']
+    coordinator = request.form['coordinator']
+    member1 = request.form['member1']
+    member2 = request.form['member2']
+    member3 = request.form['member3']
+    member4 = request.form['member4']
+    members = [member1, member2, member3, member4]
+
+    member = {'course_code': course_code, 'course_name': course_name, 'coordinator': coordinator, 'members': members}
+    #courses.insert_one(course)
+    return jsonify({'members': member})
+
+@app.route('/locf', methods=['POST']) # type: ignore
+@token_required
+def LOCF(current_user):
+    if current_user['role'] != 'admin':
+        return jsonify({'message': 'You do not have access to this page!'})
+
+    lo = request.form['lo']
+    course_name = request.form['course_name']
+    coordinator = request.form['coordinator']
+
+    locf = {'lo': lo, 'course_name': course_name, 'coordinator': coordinator}
+    #courses.insert_one(course)
+    return jsonify({'locf': locf})
+
+
+# Run the app
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+# {"program": "program", "course_code": "course_code", "course_name": "course_name", "course_type": "course_type"}
+# eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFkbWluIiwiZXhwIjoxNjgyMDU4NDAxfQ.V6YQlftOt8S20Sivc4wLRpcT3voTw5I2CnbvlE55xXw
+# /MANAGE_CONTENT = > 
